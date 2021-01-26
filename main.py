4ac13aa288f512c4de4249912578f41a0c0e5018
Chris V <43914450+flukestar@users.noreply.github.com>
2021-01-26 23:41:10 +0000

    first commit
    Initial commit
    	new file:   .gitignore
    	new file:   README
    	new file:   main.py
    	new file:   md

---
+++ main.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+import requests
+import random
+
+url = "https://www.imdb.com/chart/top"
+sauce = requests.get(url)
+soup = BeautifulSoup(sauce.content, 'html.parser')
+
+soup_trows = soup.find_all('td', class_ = 'titleColumn')
+
+movie_list = []
+for i in (soup_trows):
+    movie_list.append(i.a.text)
+
+picker = random.choice(movie_list)
+print(picker)
