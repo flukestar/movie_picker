2bf8c24376b329d2dc35dbce30f349549fab503f
Chris V <43914450+flukestar@users.noreply.github.com>
2021-01-27 00:06:01 +0000

    create function

--- main.py
+++ main.py
@@ -8,9 +8,12 @@ soup = BeautifulSoup(sauce.content, 'html.parser')
 
 soup_trows = soup.find_all('td', class_ = 'titleColumn')
 
-movie_list = []
-for i in (soup_trows):
-    movie_list.append(i.a.text)
+def movie_picker():
+    movie_list = []
+    for i in (soup_trows):
+        movie_list.append(i.a.text)
 
-picker = random.choice(movie_list)
-print(picker)
+    picker = random.choice(movie_list)
+    print(picker)
+
+movie_picker()
