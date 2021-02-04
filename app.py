c3154e224e8f2d6f04a4e5b4dfa4177e4d810e65
Chris V <43914450+flukestar@users.noreply.github.com>
2021-02-04 23:42:38 +0000

    added procfile, gunicorn

--- app.py
+++ app.py
@@ -7,21 +7,24 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    title = "Home"
+    return render_template("index.html", title=title)
 
 
 @app.route("/moviepicker")
 def moviepicker():
-    rank, title, year, poster = imdbscrapper.pick_movie()
+    title = "Movie Picker"
+    rank, film, year, poster = imdbscrapper.pick_movie()
     return render_template(
-        "moviepicker.html", film=title, rank=rank, year=year, poster=poster
+        "moviepicker.html", film=film, rank=rank, year=year, poster=poster, title=title
     )
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    title = "Login"
     if request.method == "POST":
         user = request.form["nm"]
         return redirect("/user/" + user)
     else:
-        return render_template("login.html")
+        return render_template("login.html", title=title)
