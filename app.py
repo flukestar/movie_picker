527a44b2a5c68da9b585475b40d61f0a7475c1d4
Chris V <43914450+flukestar@users.noreply.github.com>
2021-02-08 02:37:11 +0000

    worked in form and sql

--- app.py
+++ app.py
@@ -1,8 +1,25 @@
 from flask import Flask, redirect, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 import imdbscrapper
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+db = SQLAlchemy(app)
+
+
+class Watched(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    movie = db.Column(db.String(200), nullable=False)
+    watched_date = db.Column(db.String(20), nullable=False)
+    picked_by = db.Column(db.String(200), nullable=False)
+
+    def __repr__(self):
+        return "<Watched(movie='%s', watched_date='%s', picked_by='%s')>" % (
+            self.movie,
+            self.watched_date,
+            self.picked_by,
+        )
 
 
 @app.route("/")
@@ -18,6 +35,39 @@ def moviepicker():
     return render_template(
         "moviepicker.html", film=film, rank=rank, year=year, poster=poster, title=title
     )
+
+
+@app.route("/watchlist", methods=["GET", "POST"])
+def watchlist():
+    title = "Watched List"
+    if request.method == "POST":
+        film = Watched(
+            movie=request.form["movie"],
+            watched_date=request.form["watched_date"],
+            picked_by=request.form["picked_by"],
+        )
+        db.session.add(film)
+        db.session.commit()
+        return redirect("/watchlist")
+    else:
+        film_lists = Watched.query.all()
+        return render_template("watchlist.html", title=title, film_lists=film_lists)
+
+
+@app.route("/delete/<int:id>")
+def delete(id):
+    watched_id = Watched.query.get_or_404(id)
+    db.session.delete(watched_id)
+    db.session.commit()
+    return redirect("/watchlist")
+
+
+# @app.route("/add")
+# def delete(id):
+#     watched_id = Watched.query.get_or_404(id)
+#     db.session.delete(watched_id)
+#     db.session.commit()
+#     return redirect("/watchlist")
 
 
 if __name__ == "__main__":
