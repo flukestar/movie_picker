f4584bccc630adb119ea9c0f6f999f8fd0d04bfd
Chris V <43914450+flukestar@users.noreply.github.com>
2021-02-08 22:42:08 +0000

    This is a combination of 5 commits.
    Revert "Revert "added more db functions""
    
    This reverts commit cc13380f38c1b6642e84f14f6547d0eec3c15cfd.
    
    fixed revert
    
    added db
    
    cupdated requirement.txt
    
    removed old comments from app.py

--- app.py
+++ app.py
@@ -20,5 +20,33 @@ def moviepicker():
     )
 
 
+<<<<<<< HEAD
+=======
+@app.route("/watchlist", methods=["GET", "POST"])
+def watchlist():
+    title = "Watched List"
+    if request.method == "POST":
+        film = Watched(
+            movie=request.form["movie"],
+            watched_date=request.form["watched_date"],
+            picked_by=request.form["picked_by"],
+        )
+        db.session.add(film)
+        db.session.commit()
+        return redirect("/watchlist")
+    else:
+        film_lists = Watched.query.all()
+        return render_template("watchlist.html", title=title, film_lists=film_lists)
+
+
+@app.route("/delete/<int:id>")
+def delete(id):
+    watched_id = Watched.query.get_or_404(id)
+    db.session.delete(watched_id)
+    db.session.commit()
+    return redirect("/watchlist")
+
+
+>>>>>>> d17d991 (removed old comments from app.py)
 if __name__ == "__main__":
     app.run()
